037e18bc4ea0dbe1212667edee643e6780f8502f
diovana-akutsu <diovana.akutsu@bandtec.com.br>
2020-06-09 21:03:00 -0300

    arrumei jogo_katakana

--- JavaScript/jogo_katakanaJavaScript.js
+++ JavaScript/jogo_katakanaJavaScript.js
@@ -1,7 +1,6 @@
-var lista = ["ke2", "ha2", "fu2", "ho2", "e2", "i2", "u2", "o2", "ka2", "ki2", "ku2", "ko2", 
-"sa2", "shi2","su2", "se2", "so2", "ta2", "chi2", "tsu2", "te2", "to2", "na2", "nini2", "nu2",
-"ne2", "no2", "hi2", "he2", "ma2", "mi2", "mu2", "me2", "mo2", "ra2", "ri2", "ru2", "re2", "ro2",
-"ya2", "yu2", "yo2", "wa2", "wo2", "nn2"];
+var lista = ["ke", "ha", "fu", "ho", "e", "i", "u", "o", "ka", "ki", "ku", "ko", "sa", "shi",
+ "su", "se", "so", "ta", "chi", "tsu", "te", "to", "na", "nini", "nu", "ne", "no", "hi", "he", 
+ "ma", "mi", "mu", "me", "mo", "ra", "ri", "ru", "re", "ro", "ya", "yu", "yo", "wa", "wo", "nn"];
 
 function mostrar1(texto, botao) { //Duas variaveis são definidas quando chama a função
     input = input1.value;
@@ -13,7 +12,7 @@ function mostrar1(texto, botao) { //Duas variaveis são definidas quando chama a
         } else {
             sorteio = parseInt(Math.random() * lista.length);
             var sair = lista[sorteio];
-            a2.src = `./Imagens/${sair}.PNG`;
+            a.src = `./Imagens/${sair}2.PNG`;
             botao.setAttribute("onclick", `mostrar1('${sair}',this)`); // Mudar a função do onclick
             lista.splice(lista.indexOf(sair), 1); // a partir do 0 tira 1 
         }
