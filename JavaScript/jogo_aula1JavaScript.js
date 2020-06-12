c2b623fbd5253a08da5a9efae250913ce50410c0
diovana-akutsu <diovana.akutsu@bandtec.com.br>
2020-06-11 23:58:33 -0300

    Alterações

---
+++ JavaScript/jogo_aula1JavaScript.js
@@ -0,0 +1,171 @@
+function audio1(){
+    som1.play();
+}
+
+function audio2(){
+    som2.play();
+}
+
+function audio3(){
+    som3.play();
+}
+
+function audio4(){
+    som4.play();
+}
+
+function audio5(){
+    som5.play();
+}
+
+function audio6(){
+    som6.play();
+}
+
+function audio7(){
+    som7.play();
+}
+
+function audio8(){
+    som8.play();
+}
+
+function audio9(){
+    som9.play();
+}
+
+function audio10(){
+    som10.play();
+}
+
+function audio11(){
+    som11.play();
+}
+
+function audio12(){
+    som12.play();
+}
+
+function audio13(){
+    som13.play();
+}
+
+function audio14(){
+    som14.play();
+}
+
+function audio15(){
+    som15.play();
+}
+
+function audio16(){
+    som16.play();
+}
+
+function audio17(){
+    som17.play();
+}
+
+function audio18(){
+    som18.play();
+}
+
+function audio19(){
+    som19.play();
+}
+
+function audio20(){
+    som20.play();
+}
+
+function audio21(){
+    som21.play();
+}
+
+function audio22(){
+    som22.play();
+}
+
+function audio23(){
+    som23.play();
+}
+
+function audio24(){
+    som24.play();
+}
+
+function audio25(){
+    som25.play();
+}
+
+function audio26(){
+    som26.play();
+}
+
+function audio27(){
+    som27.play();
+}
+
+function audio28(){
+    som28.play();
+}
+
+function audio29(){
+    som29.play();
+}
+
+function audio30(){
+    som30.play();
+}
+
+function audio31(){
+    som31.play();
+}
+
+function audio32(){
+    som32.play();
+}
+
+function audio33(){
+    som33.play();
+}
+
+function audio34(){
+    som34.play();
+}
+
+function audio35(){
+    som35.play();
+}
+
+function audio36(){
+    som36.play();
+}
+
+function audio37(){
+    som37.play();
+}
+
+function audio38(){
+    som38.play();
+}
+
+function audio39(){
+    som39.play();
+}
+
+function audio40(){
+    som40.play();
+}
+
+function audio41(){
+    som41.play();
+}
+
+function audio42(){
+    som42.play();
+}
+
+function audio43(){
+    som43.play();
+}
