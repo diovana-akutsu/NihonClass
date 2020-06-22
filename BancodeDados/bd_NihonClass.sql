a3310bd96ce11d8f4c40db58f8b8b1c28ab377f6
diovana-akutsu <diovana.akutsu@bandtec.com.br>
2020-06-22 18:20:43 -0300

    Retoques

--- BancodeDados/bd_NihonClass.sql
+++ BancodeDados/bd_NihonClass.sql
@@ -35,11 +35,5 @@ insert into AlunosAulas values
 insert into AlunosAulas values
 (1000, 2, '500', 1);
 
-select * from alunos where email = 'Gabriel.Vieira@bandtec.com' and senha = 'gabriel18122001';
-select * from alunos where email = 'Diovana@gmail.com' and senha = '11122002';
 select * from AlunosAulas;
-delete from AlunosAulas where fk_aluno=1000;
 select * from alunos;
-
-select * from AlunosAulas where fk_aluno = '1000' and fk_aula = '1' and nota = '500';
-update AlunosAulas set fk_aula = '' and pontuacao = '' and validado = '' where fk_aluno = '';
